94e27c5801233d733488ce29d75a9eece57d319e
pablo ramos <pabloramosdev@gmail.com>
2020-08-27 19:25:49 +0200

    project skeleton

---
+++ src/test/java/com/mycomp/minibank/transactions/TransactionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycomp.minibank.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
